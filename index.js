eee63dcaa6dac78db873badc7b8f03c5e9d7fa47
anjas1239 <75497261+anjas1239@users.noreply.github.com>
2020-12-30 16:00:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const  XBOT = 'AMPIBI'; // Nama Bot Whatsapp
-const instagram = 'https://instagram.com/affis_saputro123'; // Nama Instagramlu cok
-const nomer = 'https://Wa.me/+6282334297175'; // Nomor whatsapplu cok
-const aktif = 'Tergantung kuota'; // Kapan bot lu aktif
-const groupwa = 'comming soon'; // OFFICIAL GRUP LU 1
-const youtube = 'https://youtube.com/channel/UCYKxsg7iF9a9IZyXQRNsvqw'; 
+const  XBOT = 'BOT 8 BIT'; // Nama Bot Whatsapp
+const instagram = 'g ada'; // Nama Instagramlu cok
+const nomer = 'https://Wa.me/+6282131022499'; // Nomor whatsapplu cok
+const aktif = 'Tergantung jaringan'; // Kapan bot lu aktif
+const groupwa = 'g ada'; // OFFICIAL GRUP LU 1
+const youtube = 'g ada'; 
 const qrcode = require("qrcode-terminal");
 const moment = require("moment");
 const cheerio = require("cheerio");
